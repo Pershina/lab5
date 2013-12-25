97b0b2885ba099e94e0bc6f1dc31c85961892bc5
Pershina <lenok81093@mail.ru>
2013-12-25 13:59:18 +0700

    first commit

---
+++ src/ru/pershina/lab5/Movable.java
@@ -0,0 +1,11 @@
+package ru.pershina.lab5;
+/**
+ Интерфейс для перемещния объекта на заданый вектор
+ @author Pershina Elena
+*/
+
+public interface Movable {
+    
+    void move(Point p);
+    
+}
